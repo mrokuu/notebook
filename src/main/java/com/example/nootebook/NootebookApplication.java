c3ec35cb1050f6f8f6b8f41face1dd0a593e90c3
mrokuu <mroczkowskiadammm@gmail.com>
2024-03-08 21:03:47 +0100

    first commit

---
+++ src/main/java/com/example/nootebook/NootebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.nootebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NootebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NootebookApplication.class, args);
+    }
+
+}
